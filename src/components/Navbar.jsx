2edd0a5ede5930186addb7af12146b26aaa6341c
sara niktabe <sara.niktabe@gmail.com>
2023-07-16 21:23:15 +0330

    styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () =>{
+    return(
+        <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
+            <div className="container">
+                <div className="row w-100">
+                    <div className="col">
+                        <i className="fas fa-id-badge"/>
+                        web application managment {" "}
+                        <span style={{color: "purple"}}>contact</span>
+                    </div>
+                    <div className="col">
+                    <div className="input-group mx-2 w-75">
+                    </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </nav>
+    )
+}
