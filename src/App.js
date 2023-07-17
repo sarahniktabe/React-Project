7d9ff5cdfd6859033935e976a4e5e31a6d60357d
sara niktabe <sara.niktabe@gmail.com>
2023-07-17 22:39:38 +0330

    added not found users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import Contacts from './components/contact/Contacts'
 
 const App =() => {
+  const [getContacts, setContacts]=useState([])
   return (
     <div className="App">
       <Navbar/>
-      <Contacts/>
+      <Contacts contacts={getContacts}/>
     </div>
   );
 }
