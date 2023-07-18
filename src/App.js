7cc63d083208fcdbcd689644c65ed1e7f8072704
sara niktabe <sara.niktabe@gmail.com>
2023-07-18 21:28:49 +0330

    Routed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Routes , Route, useNavigate , Navigate } from "react-router-dom";
 import "./App.css";
 import {
   AddContact,
@@ -12,12 +13,20 @@ import {
 } 
 from "./components/Index";
 
+
 const App = () => {
   const [getContacts, setContacts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/Contacts"/>} />
+        <Route path="/contacts" element={<Contacts/>} />
+        <Route path="/contacts/:contactId" element={<Contacts/>} />
+        <Route path="/contacts/edit/:contactId" element={<EditContact/>} />
+        <Route path="/notfounf" element={<NotFound/>} />
+      </Routes>
       <Contacts contacts={getContacts} loading={loading} />
     </div>
   );
